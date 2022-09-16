ec7ae11d8f3c43de426538b5bb4ceacd110e3154
caio2002quiu <90843837+caio20002quiu@users.noreply.github.com>
2022-09-15 23:31:18 -0300

    Add files via upload

---
+++ raiz_quadrada.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	
+	double x;
+	cout << " Informe a raiz que deseja calcular? \n";
+	cin>>x;
+	cout << sqrt (x) << endl;
+	
+	return 0;
+}
